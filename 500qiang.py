16cf61fc5243df31f79aead508951c6762fb1212
Shitong Zhu <zzzzzcoder@gmail.com>
2016-03-12 22:21:08 +0800

    Modified both methods, realized simple API-based and id-fixed post info retrieval function

--- 500qiang.py
+++ 500qiang.py
@@ -5,18 +5,20 @@ def access_pages(id_lst, url_prefix, access_token):
     for id in id_lst:
         r = requests.get(url_prefix+id, params=param_page)
         r_json = r.json()
-        post_id = r_json.get('id')
-        tmp_type = access_post(post_item)
-        
+        posts = r_json.get('posts').get('data')
+        for post in posts:
+            post_id = post.get('id')
+            tmp_type = access_posts(post_id, url_prefix, access_token)
+            print tmp_type
 
 def access_posts(post_id, url_prefix, access_token):
     param_post = {"access_token": access_token, "fields": "status_type"}
-    r = requests.get(url_prefix+id, params=param_post)
+    r = requests.get(url_prefix+post_id, params=param_post)
     r_json = r.json()
     post_type = r_json.get('status_type')
     return post_type
 
 if __name__ == '__main__':
     id_lst = ['125524087493943']
-    access_token = 'CAACEdEose0cBAFYSdLxR1t8RtI0ab6tE5pjfow0wdppZAVLOsZC2uuYvz1GZBeJYYTkkWsx7qdCG4WAQvZCE3qL4tjGKlW2ppHSZCpFJPJl7pwFjeZBPmektO8b41byE6tUtzPrZAMAo4Tm3qP4ac37FtZAwhQv1LbETo9ZABHRJh1GlhlTtKZBZC7xJe8BLYXAGeEF2onmr9UighQVyyJK8spV'
-    access_pages(id_lst, 'http://graph.facebook.com/v2.5/', access_token)
+    access_token = 'CAACEdEose0cBAIj8fUrZCoaZCVkg4xOwp70kR2T8U8OVNHw2BCNZBiyzIIALiQA87EZCUYQ7EuCuxioQepWWI5s7YOtNGBhpsslbsGKxtpwh4TNC74ZCz4Cvi5bWZCVsRHR9UNwX1lVzrGF0IqXgg7Y91u7zoSZChxrDiZC9hVFXA3jRTr6JVlXQYQ3tetYV6Fl4S08BRL02bUrbb2OYr5d4'
+    access_pages(id_lst, 'https://graph.facebook.com/v2.5/', access_token)
